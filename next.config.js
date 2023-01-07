96008e89b652a2f8f7383171d3038aae656b7cb7
Valentin Batiot <valentin.batiot@inli.fr>
2023-01-07 16:36:17 +0100

    tryfix3

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  swcMinify: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
